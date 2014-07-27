516a0e9c4ce04127d5012d8471b12061cd1c2e5d
Azhar Khan <azmanian.devil@gmail.com>
2014-07-27 01:59:01 -0400

    added search box and filter capability

--- app/scripts/jsx/phonebook.jsx
+++ app/scripts/jsx/phonebook.jsx
@@ -4,7 +4,7 @@ var contacts = [];
 var PhoneBook = React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function() {
-    return {contacts:[]};
+    return {contacts:[], filterText: ''};
   },
   loadContactsFromServer: function() {
     this.firebaseRef = new Firebase("https://react-phonebook.firebaseio.com/contacts/");
@@ -25,12 +25,18 @@ var PhoneBook = React.createClass({
     this.bindAsArray(new Firebase("https://react-phonebook.firebaseio.com/contacts/"), "contacts");
   },
 
+  onFilterContacts: function(query) {
+    this.setState({ filterText: query });
+  },
+
   render: function() {
     return (
       <div className="phoneBook">
-        <h2>Add New:</h2>
+        <h3>Add New</h3>
         <ContactForm onContactSubmit={this.handleContactSubmit} />
-        <ContactList contacts={this.state.contacts} />
+        <h2>List of Contacts</h2>
+        <ContactSearch filterText={this.state.filterText} filterContacts={this.onFilterContacts} />
+        <ContactList contacts={this.state.contacts} filter={this.state.filterText} />
       </div>
     );
   }
@@ -49,16 +55,32 @@ var Contact = React.createClass({
 
 var ContactList = React.createClass({
   render: function() {
-    var contactEntries = this.props.contacts.map(function(person) {
+    var query = this.props.filter;
+    var contactEntries = this.props.contacts
+    .filter(function(contact) {
+      return contact.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    })
+    .map(function(person) {
     return (
         <Contact name={person.name} number={person.number} />
       );
     });
     return (
       <div className="contactList">
-        <h2>List of Contacts</h2>
         {contactEntries}
       </div>
+    );
+  }
+});
+
+var ContactSearch = React.createClass({
+  handleFilter: function() {
+    var query = this.refs.search.getDOMNode().value;
+    this.props.filterContacts(query);
+  },
+  render: function() {
+    return (
+      <input ref="search" type="search" name="searchContact" placeholder="Search" value={this.props.filterText} onChange={this.handleFilter} />
     );
   }
 });
@@ -82,12 +104,12 @@ var ContactForm = React.createClass({
     return (
       <form className="contactForm" onSubmit={this.handleSubmit}>
         <fieldset>
-          <label for="contactName">Name</label>
+          <label>Name: </label>
           <input id="contactName" type="text" name="name" placeholder="Name" ref="name" />
-          <label for="contactNumber">Phone Number:</label>
+          <label>Phone Number: </label>
           <input id="contactNumber" type="tel" name="phone" placeholder="xxx-xxx-xxxx" ref="number" />
         </fieldset>
-        <input class="button--primary" type="submit" value="Save" />
+        <input className="button--primary" type="submit" value="Save" />
       </form>
     );
   }
